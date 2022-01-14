4748625d40a3e593950574f5b17995bd15281899
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2022-01-14 14:32:17 +0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/manjurulhoque/go-bookstore
+
+go 1.15
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,9 @@
+package routes
+
+//var RegisterBookStoreRoutes = func(router *mux.Router) {
+//	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+//	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+//	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+//	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+//	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+//}
