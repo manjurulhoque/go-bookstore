a7f682e47afbeab7a2da802c51b51a7e151ede14
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2022-01-14 20:19:03 +0600

    Define book model

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/manjurulhoque/go-bookstore/pkg/routes"
 	"log"
+	"net/http"
 )
 
 func main() {
@@ -10,4 +13,9 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
